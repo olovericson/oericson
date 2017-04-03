9590b13c89a2e1d98ca6223047d984d8ee2e7f18
olovericson <olov.ericson@nepa.com>
2017-04-03 21:30:30 +0200

    Scraping allflicks.se for netflix titles.

--- WebScraping/netflixScrape.py
+++ WebScraping/netflixScrape.py
@@ -1,46 +1,112 @@
 import requests
-from bs4 import BeautifulSoup
 import pandas as pd
 
-session = requests.session()
+def get_page_data(start):
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive',
+        'Content-Length': '1893',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': 'PHPSESSID=gjeuvksuefv3lhb8a4c2ic1ec1; SERVERID=web4; _ga=GA1.2.2007578377.1491235372; _gat=1; identifier=6e5e52f66cfce015f9ded7f7ee068549',
+        'Host': 'www.allflicks.se',
+        'Origin': 'http://www.allflicks.se',
+        'Referer': 'http://www.allflicks.se/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
 
-login_url = "https://www.netflix.com/Login"
+    data = {
+        'draw': '1',
+        'columns[0][data]': 'box_art',
+        'columns[0][name]': '',
+        'columns[0][searchable]': 'true',
+        'columns[0][orderable]': 'false',
+        'columns[0][search][value]': '',
+        'columns[0][search][regex]': 'false',
+        'columns[1][data]': 'title',
+        'columns[1][name]': '',
+        'columns[1][searchable]': 'true',
+        'columns[1][orderable]': 'true',
+        'columns[1][search][value]': '',
+        'columns[1][search][regex]': 'false',
+        'columns[2][data]': 'year',
+        'columns[2][name]': '',
+        'columns[2][searchable]': 'true',
+        'columns[2][orderable]': 'true',
+        'columns[2][search][value]': '',
+        'columns[2][search][regex]': 'false',
+        'columns[3][data]': 'genre',
+        'columns[3][name]': '',
+        'columns[3][searchable]': 'true',
+        'columns[3][orderable]': 'true',
+        'columns[3][search][value]': '',
+        'columns[3][search][regex]': 'false',
+        'columns[4][data]': 'rating',
+        'columns[4][name]': '',
+        'columns[4][searchable]': 'true',
+        'columns[4][orderable]': 'true',
+        'columns[4][search][value]': '',
+        'columns[4][search][regex]': 'false',
+        'columns[5][data]': 'available',
+        'columns[5][name]': '',
+        'columns[5][searchable]': 'true',
+        'columns[5][orderable]': 'true',
+        'columns[5][search][value]': '',
+        'columns[5][search][regex]': 'false',
+        'columns[6][data]': 'director',
+        'columns[6][name]': '',
+        'columns[6][searchable]': 'true',
+        'columns[6][orderable]': 'true',
+        'columns[6][search][value]': '',
+        'columns[6][search][regex]': 'false',
+        'columns[7][data]': 'cast',
+        'columns[7][name]': '',
+        'columns[7][searchable]': 'true',
+        'columns[7][orderable]': 'true',
+        'columns[7][search][value]': '',
+        'columns[7][search][regex]': 'false',
+        'order[0][column]': '5',
+        'order[0][dir]': 'desc',
+        'start': '0',
+        'length': '25',
+        'search[value]': '',
+        'search[regex]': 'false',
+        'movies': 'true',
+        'shows': 'false',
+        'documentaries': 'true',
+        'rating': 'netflix',
+        'min': '1900',
+        'max': '2017'
+    }
 
-result = session.get(login_url)
 
-print(result.cookies)
-soup = BeautifulSoup(result.content, 'html.parser')
+    url = "http://www.allflicks.se/wp-content/themes/responsive/processing/processing.php"
 
-inputs = list(soup.find(class_="login-form").find_all('input'))
+    data['start'] = start
 
-authURL=''
-for i in inputs:
-    if i['name']=='authURL':
-        authURL=i['value']
+    result = requests.post(url, data=data, headers=headers)
+
+    data = result.json()
+
+    return [dat['title'] for dat in data['data']]
+
+
+start = 0
+titles = list()
+while True:
+    print(start)
+
+    items = get_page_data(str(start))
+
+    if not items:
         break
 
+    titles.extend(items)
 
+    start += 25
 
-url = "https://api-global.netflix.com/iosui/warmer/9.10?appInternalVersion=9.10.0&appVersion=9.10.0&config={'summaryHasTitleTreatment':false,'summaryHasUrl':false,'thumbRatingsEnabled':'false','billboardEnabled':'true'','summaryHasHorizontalDisplayArt':false,'summaryHasSynopsis':false,'showMoreDirectors':true,'kidsParityEnabled':'false','roarEnabled':'true','summaryHasStoryArt':false,'summaryHasPreviewStoryArt':false,'kidsBillboardEnabled':'true','bigRowEnabled':'true','summaryHasId':false,'warmerHasGenres':true,'useSecureImages':true}&device_type=NFAPPL-02-&esn=esn&id=genre_id&idiom=phone&iosVersion=10.2.1&isTablet=false&kids=false&languages=se-Sv&locale=se-sv&maxDeviceWidth=414&method=get&model=saget&modelType=IPHONE9-2&odpAware=true&pageType=page_type&pathFormat=graph&pixelDensity=3.0&progressive=false&responseFormat=json&type=gallery-ath"
+df = pd.DataFrame({'title': titles})
 
-data = {
-    "email": "olov_ericson@hotmail.com",
-    "password": "leksandstars1",
-    "authURL": authURL,
-    "RememberMe": "true"
-}
-
-
-header = {
-    'User-Agent': "NodeScrape",
-    'Cookie': 'NetflixId=3DBQAOAAEBEHoUpOw42U1EMOnTR0UU9U;SecureNetflixId=v%3D2%26mac%3DAQEAEQABABSypzgsIQYfTYhaHCXQv48IzuIMsjGqR9o.%26dt%3D1491162221899'
-}
-
-result = session.post(login_url, data=data, headers=header)
-cookies = result.headers['set-cookie']
-
-result = session.get("https://www.netflix.com/WiViewingActivity", headers = header)
-
-soup = BeautifulSoup(result.content, 'html.parser')
-
-print(soup.prettify())
+df.to_csv('data/netflix_movies.csv')
